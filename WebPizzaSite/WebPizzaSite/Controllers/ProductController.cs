57e961f8d3d09c175436467ebebd97d7f2320ff9
andrxpie <andrxpie@gmail.com>
2024-08-25 10:17:59 +0300

    Add 2 tables

---
+++ WebPizzaSite/WebPizzaSite/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using WebPizzaSite.Data;
+using WebPizzaSite.Models.Product;
+
+namespace WebPizzaSite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly PizzaDbContext _context;
+        private readonly IMapper _mapper;
+        public ProductController(PizzaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _context.Products
+                .ProjectTo<ProductItemViewModel>(_mapper.ConfigurationProvider)
+                .ToList();
+            return View(list);
+        }
+    }
+}
